7330cd8493bea00f2e037d54033c4767989dd4e0
Johan Smarius <jsmarius@hotmail.com>
2024-01-05 12:48:33 +0100

    Add project files.

---
+++ UnitTestUitleg/Order.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UnitTestUitleg
+{
+    public class Order
+    {
+        public List<OrderItem> OrderItems { get; set; }
+        public string Customer { get; set; }
+    }
+}
